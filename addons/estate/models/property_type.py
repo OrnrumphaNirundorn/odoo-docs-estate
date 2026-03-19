4c3c757f587d4b9b604b490f0103dfd5845da96c
Ornrumpha Nirundorn <shisha31106@gmail.com>
2026-03-20 04:51:41 +0700

    Finished till list

--- addons/estate/models/property_type.py
+++ addons/estate/models/property_type.py
@@ -4,9 +4,20 @@ class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type"
 
+    sequence = fields.Integer(default=10)
+    _order = "sequence, name"
+
+    
+    
     name = fields.Char(required=True)
 
     _check_name_unique_sql = models.Constraint(
         'UNIQUE(name)',
         'The property type name must be unique.'
+    )
+
+    property_ids = fields.One2many(
+        "estate.property",
+        "property_type_id",
+        string="Properties"
     )
